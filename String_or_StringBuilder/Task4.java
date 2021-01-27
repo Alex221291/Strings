331b85ae52f52a4cf6224aa1e8bfad0c691433c9
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2021-01-27 21:49:12 +0300

    Задачи_Strings

--- String_or_StringBuilder/Task4.java
+++ String_or_StringBuilder/Task4.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args)  {
         Scanner console = new Scanner(System.in);
-        System.out.println("Enter string");
         String s1 = new String("информатика");
         String s2 = s1.charAt(7) + s1.substring(3, 5) + s1.charAt(7);
         System.out.println(s2);
